ce953ef8e4fa40ed6fd98dfe694f6181e7a033ef
guhpissai <gustavopissai3d@gmail.com>
2023-04-04 17:28:06 -0300

    Implementando 100% dos testes do Footer e Header

--- src/index.js
+++ src/index.js
@@ -4,23 +4,13 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import RecipeProvider from './provider/RecipeProvider';
-import HeaderProvider from './provider/HeaderProvider';
-import SearchBarProvider from './provider/SearchBarProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <SearchBarProvider>
-      <RecipeProvider>
-        <HeaderProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </HeaderProvider>
-      </RecipeProvider>
-    </SearchBarProvider>,
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   );
 
 // If you want your app to work offline and load faster, you can change
