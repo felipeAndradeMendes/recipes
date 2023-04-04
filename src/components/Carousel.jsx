1ca83e783bb0472f64f5a2817857babc91c21d76
bruno <bruno.costa1203@yahoo.com>
2023-04-04 17:43:19 -0300

    Lint corrigido

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function Carousel({ sugestions }) {
-  const [currentCard, setCurrentCard] = useState(0);
-  const handleNextCard = () => {
-    setCurrentCard(currentCard === sugestions.length - 1 ? 0 : currentCard + 1);
-  };
+// function Carousel({ sugestions }) {
+//   const [currentCard, setCurrentCard] = useState(0);
+//   const handleNextCard = () => {
+//     setCurrentCard(currentCard === sugestions.length - 1 ? 0 : currentCard + 1);
+//   };
 
-  const handlePrevCard = () => {
-    setCurrentCard(currentCard === 0 ? sugestions.length - 1 : currentCard - 1);
-  };
-  return (
-    <div>
-      {sugestions.splice(0, 6).map((sugestion, index) => (
-        <div key={ sugestion.idMeal || sugestion.idDrink }>
-          <img
-            src={ sugestion.strMealThumb || sugestion.strDrinkThumb }
-            alt={ sugestion.strMeal || sugestion.strDrink }
-            data-testid={ `${index}-recomendation-card` }
-          />
-          <p data-testid={ `${index}-recomendation-title` }>
-            {sugestion.strMeal || sugestion.strDrink}
-          </p>
-        </div>
-      ))}
-      <button
-        type="button"
-        onClick={ () => handlePrevCard() }
-      >
-        Prev
-      </button>
-      <button
-        type="button"
-        onClick={ () => handleNextCard() }
-      >
-        Next
-      </button>
-    </div>
-  );
-}
+//   const handlePrevCard = () => {
+//     setCurrentCard(currentCard === 0 ? sugestions.length - 1 : currentCard - 1);
+//   };
+//   return (
+//     <div>
+//       {sugestions.splice(0, 6).map((sugestion, index) => (
+//         <div key={ sugestion.idMeal || sugestion.idDrink }>
+//           <img
+//             src={ sugestion.strMealThumb || sugestion.strDrinkThumb }
+//             alt={ sugestion.strMeal || sugestion.strDrink }
+//             data-testid={ `${index}-recomendation-card` }
+//           />
+//           <p data-testid={ `${index}-recomendation-title` }>
+//             {sugestion.strMeal || sugestion.strDrink}
+//           </p>
+//         </div>
+//       ))}
+//       <button
+//         type="button"
+//         onClick={ () => handlePrevCard() }
+//       >
+//         Prev
+//       </button>
+//       <button
+//         type="button"
+//         onClick={ () => handleNextCard() }
+//       >
+//         Next
+//       </button>
+//     </div>
+//   );
+// }
 
-export default Carousel;
+// export default Carousel;
