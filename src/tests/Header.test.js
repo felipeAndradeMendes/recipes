3185befa27b72cd092fe825e0ff08c10815ba46e
Felipe Mendes <felipemadruga@gmail.com>
2023-04-04 14:59:57 -0300

    Refatora e conclui testes do Header, reqs 07, 08 e 09

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,31 +1,149 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
 import Header from '../components/Header';
 import HeaderProvider from '../provider/HeaderProvider';
+import SearchBarProvider from '../provider/SearchBarProvider';
 
 describe('Testes do Componente Header', () => {
+  const profileLinkBtn = 'profile-top-btn';
+  const searchTopBtn = 'search-top-btn';
   test('Os componentes são renderizados', () => {
+    const history = createMemoryHistory();
+    history.push('/meals');
     render(
-      <HeaderProvider>
-        <BrowserRouter>
-          <Header />
-        </BrowserRouter>
-      </HeaderProvider>,
+      <Router history={ history }>
+        <SearchBarProvider>
+          <HeaderProvider>
+            <Header />
+          </HeaderProvider>
+        </SearchBarProvider>
+      </Router>,
     );
-    const profileIcon = screen.getByTestId('profile-top-btn');
-    const searchIcon = screen.getByTestId('search-top-btn');
+    // console.log(history.location.pathname);
+
+    // act(() => {
+    //   history.push('/meals');
+    // });
+    expect(history.location.pathname).toBe('/meals');
+
+    screen.logTestingPlaygroundURL();
+
+    const profileIcon = screen.getByTestId(profileLinkBtn);
+    const searchIcon = screen.getByTestId(searchTopBtn);
     expect(profileIcon).toBeInTheDocument();
     expect(searchIcon).toBeInTheDocument();
 
     userEvent.click(searchIcon);
-    expect(screen.getByText(/barra de busca/i)).toBeInTheDocument();
+    const searchInput = screen.getByTestId('search-input');
+    expect(searchInput).toBeInTheDocument();
+
+    // expect(screen.getByText(/barra de busca/i)).toBeInTheDocument();
+    // userEvent.click(searchIcon);
+    // expect(screen.queryByText(/barra de busca/i)).not.toBeInTheDocument();
+
+    // userEvent.click(profileIcon);
+    // expect(screen.getByText(/profile/i)).toBeInTheDocument();
+  });
+
+  test('Os componentes são renderizados em /drinks', () => {
+    const history = createMemoryHistory();
+    history.push('/drinks');
+    render(
+      <Router history={ history }>
+        <SearchBarProvider>
+          <HeaderProvider>
+            <Header />
+          </HeaderProvider>
+        </SearchBarProvider>
+      </Router>,
+    );
+
+    expect(history.location.pathname).toBe('/drinks');
+
+    screen.logTestingPlaygroundURL();
+
+    const profileIcon = screen.getByTestId(profileLinkBtn);
+    const searchIcon = screen.getByTestId(searchTopBtn);
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).toBeInTheDocument();
+
     userEvent.click(searchIcon);
-    expect(screen.queryByText(/barra de busca/i)).not.toBeInTheDocument();
+    const searchInput = screen.getByTestId('search-input');
+    expect(searchInput).toBeInTheDocument();
+  });
+
+  test('Os componentes são renderizados em /profile', () => {
+    const history = createMemoryHistory();
+    history.push('/profile');
+    render(
+      <Router history={ history }>
+        <SearchBarProvider>
+          <HeaderProvider>
+            <Header />
+          </HeaderProvider>
+        </SearchBarProvider>
+      </Router>,
+    );
+
+    expect(history.location.pathname).toBe('/profile');
+
+    screen.logTestingPlaygroundURL();
+
+    const profileIcon = screen.getByTestId(profileLinkBtn);
+    const searchIcon = screen.queryByTestId(searchTopBtn);
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+  });
+
+  test('Os componentes são renderizados em /done-recipes', () => {
+    const history = createMemoryHistory();
+    history.push('/done-recipes');
+    render(
+      <Router history={ history }>
+        <SearchBarProvider>
+          <HeaderProvider>
+            <Header />
+          </HeaderProvider>
+        </SearchBarProvider>
+      </Router>,
+    );
+
+    expect(history.location.pathname).toBe('/done-recipes');
+
+    screen.logTestingPlaygroundURL();
+
+    const profileIcon = screen.getByTestId(profileLinkBtn);
+    const searchIcon = screen.queryByTestId(searchTopBtn);
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+  });
+
+  test('Os componentes são renderizados em /favorite-recipes', () => {
+    const history = createMemoryHistory();
+    history.push('/favorite-recipes');
+    render(
+      <Router history={ history }>
+        <SearchBarProvider>
+          <HeaderProvider>
+            <Header />
+          </HeaderProvider>
+        </SearchBarProvider>
+      </Router>,
+    );
+
+    expect(history.location.pathname).toBe('/favorite-recipes');
+
+    screen.logTestingPlaygroundURL();
+
+    const profileIcon = screen.getByTestId(profileLinkBtn);
+    const searchIcon = screen.queryByTestId(searchTopBtn);
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
 
     userEvent.click(profileIcon);
-    expect(screen.getByText(/profile/i)).toBeInTheDocument();
-    screen.logTestingPlaygroundURL();
+    expect(history.location.pathname).toBe('/profile');
   });
 });
