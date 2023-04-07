346b4eb968b4f5bdf558bcfc9ba5a4bff8024434
Felipe Mendes <felipemadruga@gmail.com>
2023-04-07 18:13:47 -0300

    Corrige possivel erro na pagina Done Recipes

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -36,16 +36,10 @@ function FavoriteRecipes() {
   }
 
   function handleFavoriteClick(recipeId) {
-    // console.log('cliquei');
-    // console.log(recipeId);
-    // console.log('FAVORITE FROM LOCAL STORAGE:', favoriteFromLocalStorage);
-
     const newFavoritesArr = favorites
       .filter((recipe) => Number(recipe.id) !== Number(recipeId));
 
-    console.log('NEW FAVORITE:', newFavoritesArr);
     localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoritesArr));
-    // setFavorites(newFavoritesArr);
     const favoriteFromLocalStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
     setFavorites(favoriteFromLocalStorage);
   }
